b5eb7e0e6ea27c2a4bf76d564d5ef23917aa4fd6
iamayac <ayachristia@gmail.com>
2025-02-15 23:28:37 +0100

    wai-ARIA ADDED

--- scripts/details.js
+++ scripts/details.js
@@ -16,7 +16,7 @@ fetch(`/data/destinations.json`)
     const destinationEl = document.createElement("section");
     destinationEl.classList.add("destination");
     destinationEl.innerHTML = `
-    <section class="destination__left">
+    <section class="destination__left" aria-label="destinationImage">
     
     <section class="destination__image-container">
     <img src="${destination.image}" alt="${
@@ -24,26 +24,30 @@ fetch(`/data/destinations.json`)
     }" class="destination__image">
     </section>
     <p class="destination__icon">
-    <span class="material-symbols-outlined ">favorite</span> 
+    <span class="material-symbols-outlined " aria-label="chooseFavorite" role="button">favorite</span> 
     Favorit
     </p>
     </section>
 
     <section class="destination__right">
-    <h1 class="destination__headline" aria-labelledby="overskrift">${
+    <h1 class="destination__headline" id="overskrift">${
       destination.destination
     }</h1>
-    <h2 class="destination__subtitle" id="overskrift">${destination.title}</h2>
+    <h2 class="destination__subtitle" aria-describedby="overskrift" aria-label="">${
+      destination.title
+    }</h2>
     
-    <p class="destination__review">
-    <span class="destination__review-procent">100</span>% af nylige gæster har givet indtjekningen en <span class="destination__review-stjerner">5</span>stjernet vurdering.
+    <p class="destination__review" aria-label="costumerReview">
+    <span class="destination__review-procent">100</span>% of new guests has given the place a <span class="destination__review-stjerner">5</span> stars rating.
     </p>
-    <section class="destination__details">
-      <p class="destination__details-text">${destination.text}</p>
-      <h2 class="destination__details-headline">Facilities</h2>
-      <ul class="destination__details-facilities">
+    <section class="destination__details" aria-labelledby="destinationInfo">
+      <p class="destination__details-text" id="destinationInfo">${
+        destination.text
+      }</p>
+      <h2 class="destination__details-headline" id="overskrift" aria-label="facilities">Facilities</h2>
+      <ul class="destination__details-facilities" aria-describedby="overskrift">
     ${destination.facilities.map(function (facility) {
-      return `<li class="destination__facilitiesitem">${facility}</li>`;
+      return `<li class="destination__facilitiesitem" aria-label="facility">${facility}</li>`;
     })}</ul>
     </section>
 
